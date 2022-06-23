e1b1fd281929981f6676febe6e4a15c71566a25f
Akvigi <94718260+Akvigi@users.noreply.github.com>
2022-06-23 16:38:50 +0000

    Deploying to gh-pages from @ Akvigi/goit-js-hw-11@445f717e2285f5174e661651591899433644bc25 🚀

---
+++ index.00318d75.js
@@ -0,0 +1,2 @@
+document.querySelector(".search-form"),document.querySelector(".img-container");
+//# sourceMappingURL=index.00318d75.js.map
---
+++ index.8a3c2ad8.js
@@ -0,0 +1,2 @@
+document.querySelector(".search-form"),document.querySelector(".img-container");
+//# sourceMappingURL=index.8a3c2ad8.js.map
