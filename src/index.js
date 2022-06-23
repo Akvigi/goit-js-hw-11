08f090da82b51b1541cb9df4b9fbc463e387cc6c
Nikita P <94718260+Akvigi@users.noreply.github.com>
2022-06-23 23:37:42 +0300

    3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-
+// import { Notify } from 'notiflix/build/notiflix-notify-aio'
 
 
 const form = document.querySelector(".search-form");
 const container = document.querySelector(".img-container");
 
+const search = form.elements.searchQuery
+let markup;
+
+console.log(search);
+
+const fetchImg = async () => {
+    const query = await fetch(`https://restcountries.com/v3.1/name/${search.value}`)
+    const imgs = await response.json()
+    return console.log(imgs)
+}
+// const query = async () => {
+//     return fetchImg().then(imgs => console.log(imgs))
+// }
+
+search.addEventListener("keydown", () => console.log(search.value))
