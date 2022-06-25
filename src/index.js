daf781799645bf8f80e79713b4486ad901ee6f53
Nikita P <94718260+Akvigi@users.noreply.github.com>
2022-06-25 16:55:32 +0300

    3

--- src/index.js
+++ src/index.js
@@ -4,18 +4,71 @@
 const form = document.querySelector(".search-form");
 const container = document.querySelector(".img-container");
 
-const search = form.elements.searchQuery
+const searchInput = form.elements.searchQuery
 let markup;
 
-console.log(search);
+const searchParams = new URLSearchParams({
+    page: 1,
+    per_page: 20,
+    key: "28235798-10089aa8a519f6d1c62a23eff",
+    q: searchInput.value, 
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: true,
+})
+
+function doMarkupForImgs(items) {
+    markup = items.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>{
+        return `<div class='img-block'>
+                    <img src="${webformatURL}" alt="${tags}" loading="lazy">
+                        <div class="info">
+                            <p class="info-item">
+                                <b>Likes</b>
+                                ${likes}
+                            </p>
+                            <p class="info-item">
+                                <b>Views</b>
+                                ${views}
+                            </p>
+                            <p class="info-item">
+                                <b>Comments</b>
+                                ${comments}
+                            </p>
+                            <p class="info-item">
+                                <b>Downloads</b>
+                                ${downloads}
+                            </p>
+                        </div>
+                </div> `;
+    }).join('')
+};
+
+;
 
 const fetchImg = async () => {
-    const query = await fetch(`https://restcountries.com/v3.1/name/${search.value}`)
-    const imgs = await response.json()
-    return console.log(imgs)
+    const query = await fetch(`https://pixabay.com/api/${searchParams}`)
+    const imgs = await query.json()
+    if (!imgs.ok) {
+        throw new Error(imgs.status);
+    }
+    return imgs
 }
-// const query = async () => {
-//     return fetchImg().then(imgs => console.log(imgs))
-// }
 
-search.addEventListener("keydown", () => console.log(search.value))
+function search() {
+    container.innerHTML = ""
+    fetchImg().then( imgs => { 
+        doMarkupForImgs(imgs)
+    })
+}
+
+// const query = async () => {
+//   try {
+//
+//     const users = await fetchImg();
+//     console.log(users);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// };
+
+form.addEventListener("submit", fetchImg)
