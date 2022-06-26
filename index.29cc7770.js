22181831e3f45401af66f4dcd45ddc12dcbedb60
Akvigi <94718260+Akvigi@users.noreply.github.com>
2022-06-26 15:26:04 +0000

    Deploying to gh-pages from @ Akvigi/goit-js-hw-11@bd56f24dd4001aa96896c6ebe9474e49b5905590 🚀

--- index.29cc7770.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".search-form"),o=(document.querySelector(".img-container"),e.elements.searchQuery);console.log(o);o.addEventListener("keydown",(()=>console.log(o.value)));
-//# sourceMappingURL=index.29cc7770.js.map
